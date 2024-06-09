a672e1999ef5e6d7f18882bf93f1e3dcaba7208e
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-09 00:12:18 -0400

    branch secundaria

--- ex004.py
+++ ex004.py
@@ -7,7 +7,12 @@ class Retangulo:
 
     def area(self):
         return self.altura*self.largura
+    
+    def perimetro(self):
+        return (self.altura*self.largura)*2
         
 retangulo1 = Retangulo(30,60)
+perimetro1 = Retangulo(55, 89)
 
 print(retangulo1.area())
+print(perimetro1.perimetro())
