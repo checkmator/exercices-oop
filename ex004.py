a34a0ba897fd5fa929d72145c6cb72e55a5458f5
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-09 00:06:04 -0400

    resolvido

--- ex004.py
+++ ex004.py
@@ -1,1 +1,13 @@
 #Crie uma classe Retangulo que tenha atributos largura e altura e métodos para calcular a área e o perímetro do retângulo.
+
+class Retangulo:
+    def __init__(self, altura, largura):
+        self.altura = altura
+        self.largura = largura
+
+    def area(self):
+        return self.altura*self.largura
+        
+retangulo1 = Retangulo(30,60)
+
+print(retangulo1.area())
