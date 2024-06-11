74727bec4d3d2a90b2362767f5341b56cc064a29
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-11 18:01:45 -0400

    init

---
+++ ex009.py
@@ -0,0 +1,4 @@
+#Crie uma classe Cachorro com os atributos nome, idade e raca. 
+#Adicione métodos para exibir as informações do cachorro e calcular a idade do cachorro em anos humanos 
+#(multiplique a idade do cachorro por 7).
+
