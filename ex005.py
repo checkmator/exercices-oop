5549a49442a1b574eeae5a1b4dc3464986868071
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-10 23:29:39 -0400

    resposta final

--- ex005.py
+++ ex005.py
@@ -2,22 +2,30 @@
 #Inclua métodos para depositar e sacar dinheiro, além de consultar o saldo.
 
 class ContaBancaria:
-    def __init__(self, numero, titular, saldo):
+    def __init__(self, numero, titular, saldo = 0): #acertei
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
 
     def depositar(self, valor):
-        return self.saldo + valor
+        self.saldo += valor
 
     def sacar(self, valor):
-        return self.saldo - valor
+        if valor <= self.saldo:
+            self.saldo -= valor
+        else:
+            print("Saldo insuficiente")
 
-    def consultar(self):
-        return self.saldo
+    def consultar(self): #acertei
+        return self.saldo 
     
     #fim
 
+# Teste da classe
+conta1 = ContaBancaria("12345-6", "Maria", 1000)
+conta1.depositar(500)
+conta1.sacar(200)
+print(conta1.consultar())  # Saída: 1300
 
 
 
