3696745da0f1eb8ec1b328dd4bfdba2b9f363641
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-10 23:35:31 -0400

    fim

--- ex006.py
+++ ex006.py
@@ -1,2 +1,17 @@
 #Crie uma classe Funcionario com os atributos nome, salario e cargo. 
 #Inclua métodos para obter um aumento de salário e exibir as informações do funcionário.
+
+class Funcionario:
+    def __init__(self, nome, salario, cargo):
+        self.nome = nome
+        self.salario = salario
+        self.cargo = cargo
+    
+    def aumento_salario(self, salario, aumento):
+        self.salario += aumento
+        print(aumento)
+
+
+dados = Funcionario("joao", 1000, "adm")
+
+dados.aumento_salario(1000, 500)
