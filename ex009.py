623245fb14eda2476f1aa4b128d0d4deec6efe96
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-11 20:23:29 -0400

    final

--- ex009.py
+++ ex009.py
@@ -2,3 +2,21 @@
 #Adicione métodos para exibir as informações do cachorro e calcular a idade do cachorro em anos humanos 
 #(multiplique a idade do cachorro por 7).
 
+class Cachorro:
+    def __init__(self, nome, idade, raca):
+        self.nome = nome
+        self.idade = idade
+        self.raca = raca
+
+    def calcular_idade(self):
+        idade_humano = self.idade*7
+        return idade_humano
+
+    def exibir_infos(self):
+        return f"Nome: {self.nome}, Idade: {self.idade},Idade em anos humanos: {}, raca: {self.raca}"
+
+dog1 = Cachorro("Perola", 2, "vira lata")
+
+print(dog1.calcular_idade())
+print(dog1.exibir_infos())
+
