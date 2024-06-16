ecd4ea807d15d3c45f210da84a221d92ecb409c8
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-15 23:41:49 -0400

    init

---
+++ ex011.py
@@ -0,0 +1,2 @@
+#Crie uma classe Produto com os atributos nome e preco. 
+#Em seguida, crie uma classe Estoque que gerencie uma lista de produtos, permitindo adicionar e remover produtos, além de calcular o valor total do estoque.
