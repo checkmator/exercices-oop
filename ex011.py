76fdaf4aa6d86afa1a94cab02c7735ddaed91986
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-15 23:50:44 -0400

    fim

--- ex011.py
+++ ex011.py
@@ -6,16 +6,24 @@ class Produto:
         self.nome = nome
         self.preco = preco
     
-class Estoque(Produto):
-    def exibir_produtos():
-        pass
+class Estoque:
+    def __init__(self):
+        self.produtos = []
+    
+    def adicionar_produto(self, produto):
+        self.produtos.append(produto)
+    
+    def remover_produto(self, produto):
+        self.produtos.remove(produto)
+    
+    def calcular_valor_total(self):
+        return sum(produto.preco for produto in self.produtos)
 
-    def adicionar_produtos():
-        pass
-
-    def remover_produtos():
-        pass
-
-    def calcular_valor_total():
-        pass
+# Teste das classes
+produto1 = Produto("Arroz", 20)
+produto2 = Produto("Feijão", 10)
+estoque = Estoque()
+estoque.adicionar_produto(produto1)
+estoque.adicionar_produto(produto2)
+print(estoque.calcular_valor_total())  # Saída: 30
     
