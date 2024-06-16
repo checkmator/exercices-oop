47693b149d5507bd4c4f5823b1b2e21c5ceebf95
João Pedro Gianezini <84107419+checkmator@users.noreply.github.com>
2024-06-15 23:55:46 -0400

    .

--- ex012.py
+++ ex012.py
@@ -1,1 +1,5 @@
 #Crie uma classe Calculadora com métodos para as operações básicas (adição, subtração, multiplicação e divisão).
+
+class Calculadora:
+    def __init__(self, adicao, subtracao, multiplicacao, divisao):
+        pass
